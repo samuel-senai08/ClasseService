da8b7b0d9469d062c59836c296aff4cfaef4cc30
Samuel <bonifaciosamuel2107@gmail.com>
2026-02-24 12:25:31 -0300

    Update RacaService.java

--- RacaService.java
+++ RacaService.java
@@ -1,5 +1,54 @@
 package com.petshop.demo.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.petshop.demo.Entity.Raca;
+import com.petshop.demo.Repository.RacaRepository;
+
+@Service
 public class RacaService {
+	
+	private final RacaRepository racaRepository;
+	
+	public RacaService(RacaRepository racaRepository) {
+		this.racaRepository = racaRepository;
+		
+	}
+	
+	public Raca saveRaca(Raca raca) {
+		return racaRepository.save(raca);
+		
+	}
+	public Raca getRacaById(Long id) {
+		return racaRepository.findById(id).orElse(null);
+		
+	}
+	
+	public List<Raca> getAllRaca() {
+		return racaRepository.findAll();
+	}
+	public Raca alteraRaca(Long id, Raca alteraRaca) {
+		Optional<Raca> existeRaca = racaRepository.findById(id);
+		if (existeRaca.isPresent()) {
+			alteraRaca.setId(id);
+			return racaRepository.save(alteraRaca);
+		}else {
+			return null;
+			
+		}
+	}
+	public boolean deleteRaca(Long id) {
+		Optional<Raca> existeRaca = racaRepository.findById(id);
+		if(existeRaca.isPresent()) {
+			racaRepository.deleteById(id);
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
+
