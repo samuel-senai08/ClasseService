963c196a920c64add6776d6d512de5733bd6a5c0
Samuel <bonifaciosamuel2107@gmail.com>
2026-02-24 12:26:55 -0300

    Implement TutorService with CRUD operations

--- TutorService.java
+++ TutorService.java
@@ -1,5 +1,52 @@
 package com.petshop.demo.Service;
 
-public class TutorService {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.petshop.demo.Entity.Tutor;
+import com.petshop.demo.Repository.TutorRepository;
 
+
+
+@Service
+public class TutorService {
+	private final TutorRepository tutorRepository;
+
+	public TutorService(TutorRepository tutorRepository) {
+	this.tutorRepository = tutorRepository;
+	}
+	
+	public Tutor saveTutor (Tutor tutor) {
+		return tutorRepository.save(tutor);
+	}
+	
+	public Tutor getTutorById(Long id) {
+		return tutorRepository.findById(id).orElse(null);
+	}
+	public List<Tutor> getAllTutor() {
+		return tutorRepository.findAll();
+	}
+	public Tutor alteraTutor(Long id, Tutor alteraTutor) {
+		Optional<Tutor> existeTutor = tutorRepository.findById(id);
+		if (existeTutor.isPresent()) {
+			alteraTutor.setId(id);
+			return tutorRepository.save(alteraTutor);
+		}
+		else {
+			return null;
+		}
+	}
+	public boolean deleteTutor(Long id) {
+		Optional<Tutor> existeTutor = tutorRepository.findById(id);
+		if (existeTutor.isPresent()) {
+			tutorRepository.deleteById(id);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 }
+
+	
